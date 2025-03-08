d950bab48926a4194500e25a3952ba8b47040338
Parasdeveloper8 <parasprajapat8th@gmail.com>
2025-03-08 23:48:08 +0530

    Day 2 of php

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+   //echo("Hello world"); single arg
+   echo "Hello World","paras","gopher";  //multiple arg
+?>
---
+++ variables.php
@@ -0,0 +1,8 @@
+<?php
+//variables in php are defined with $
+$firstname = 'paras';
+$lastName = 'prajapat';
+echo $firstname . $lastName; //concatination of two strings
+
+echo $firstname . ' '. $lastName //creating space
+?>
