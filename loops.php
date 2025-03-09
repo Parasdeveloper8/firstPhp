071cd64b4069f538d33b4f15f9f592a9261cba3d
Parasdeveloper8 <parasprajapat8th@gmail.com>
2025-03-09 14:15:21 +0530

    loops completed

---
+++ loops.php
@@ -0,0 +1,22 @@
+<?php
+   //loops := for,while,do-while
+
+   //for loop
+   for($i = 0;$i < 5;$i++){
+    echo $i;
+   }
+   //while loop
+   $i = 0;
+   while($i < 5){
+    echo $i;
+    $i++;
+   }
+
+   //do-while
+   $v = 0;
+   do{
+      echo $v;
+      $v++;
+   }while($v < 5);
+
+?>
